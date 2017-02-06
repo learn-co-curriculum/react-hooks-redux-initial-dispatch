c2a0fe9deb291cf472b4a2afa138eb1423c1e7cc
Jeff Katz <jeffreyerickatz@gmail.com>
2017-02-06 11:03:31 -0500

    add js file to codealong

---
+++ reducer.js
@@ -0,0 +1,19 @@
+let state = {counter: 0};
+
+function changeState(state, action){
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
+  }
+
+function dispatch(action){
+    state = changeState(state, action)
+    render()
+}
+
+function render(){
+    document.setInnerHtml = state.counter
+}
